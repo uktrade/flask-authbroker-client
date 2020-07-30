3e17bdc5c9bbe638b2e23fe89e90d0a35a0a5a37
Mark Henwood <mcbhenwood@icloud.com>
2020-07-30 10:46:52 +0100

    Enable 'code' param to be sent to Mock SSO

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='flask-authbroker-client',
-      version='0.1',
+      version='0.2',
       description='A blueprint to integrate with the DIT Authbroker client',
       author='Lyndon Garvey',
       author_email='lyndon.garvey@digital.trade.gov.uk',
