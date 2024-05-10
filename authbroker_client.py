fd0a43c79d643940e8d218d6fdfb996feca61669
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2024-05-10 15:02:02 +0100

    BREAKING CHANGE: migrates away from deprecated flask client, uses new authlib library

--- authbroker_client.py
+++ authbroker_client.py
@@ -1,34 +1,32 @@
 import logging
-import warnings
 from functools import wraps
 from urllib.parse import urljoin, urlparse
 
-from werkzeug import security
-from flask import Blueprint, redirect, url_for, session, request, current_app
 
-# ATTENTION: Flask-OAuthLib is deprecated
-# Authlib (https://github.com/lepture/authlib) should be used instead
-try:
-    # newer projects
-    from authlib.integrations.flask_client import OAuth
-except ImportError:
-    # this is a fallback import to allow existing projects time to update Flask/Werkzeug
-    warnings.warn("deprecated", DeprecationWarning)
-    from flask_oauthlib.client import OAuth
+from flask import Blueprint, redirect, url_for, session, request, current_app
+from authlib.integrations.flask_client import OAuth
 
 
 __all__ = ('authbroker_blueprint', 'NotAuthenticatedError', 'login_required',
            'protect_all_views', 'get_profile', 'is_authenticated')
 
-
-authbroker_blueprint = Blueprint('auth', __name__)
-
-logger = logging.getLogger(__name__)
-oauth = OAuth(current_app)
-authbroker_client = None
-
 TOKEN_SESSION_KEY = '_authbroker_token'
 PROFILE_PATH = '/api/v1/user/me/'
+
+authbroker_blueprint = Blueprint('auth', __name__)
+logger = logging.getLogger(__name__)
+
+
+def get_session_token(_):
+    """
+        Required by OAuth client
+        - fetches the token from session and not from db
+        - needs a param
+    """
+    return session.get(TOKEN_SESSION_KEY)
+
+
+oauth = OAuth(current_app, fetch_token=get_session_token)
 
 
 class NotAuthenticatedError(Exception):
@@ -36,35 +34,27 @@ class NotAuthenticatedError(Exception):
 
 
 def _get_client():
-    global authbroker_client, get_token
-
-    if not authbroker_client:
+    try:
+        client = oauth.authbroker
+        return client
+    except AttributeError:
         conf = current_app.config
-
         base_url = conf['ABC_BASE_URL']
+        client_id = conf['ABC_CLIENT_ID']
+        client_secret = conf['ABC_CLIENT_SECRET']
+        access_token_url = urljoin(base_url, '/o/token/')
+        authorize_url = urljoin(base_url, '/o/authorize/')
 
-        request_token_params = {
-            'state': lambda: security.gen_salt(10)
-        }
-        test_sso_return_token = conf.get('ABC_TEST_SSO_RETURN_ACCESS_TOKEN')
-        if test_sso_return_token:
-            request_token_params['code'] = test_sso_return_token
-
-        authbroker_client = oauth.remote_app(
-            'authbroker',
-            base_url=base_url,
-            request_token_url=None,
-            access_token_url=urljoin(base_url, '/o/token/'),
-            authorize_url=urljoin(base_url, '/o/authorize/'),
-            consumer_key=conf['ABC_CLIENT_ID'],
-            consumer_secret=conf['ABC_CLIENT_SECRET'],
-            access_token_method='POST',
-            request_token_params=request_token_params,
+        oauth.register(
+            name='authbroker',
+            client_id=client_id,
+            client_secret=client_secret,
+            access_token_url=access_token_url,
+            authorize_url=authorize_url,
+            api_base_url=base_url,
+            client_kwargs=None,
         )
-
-        get_token = authbroker_client.tokengetter(get_token)
-
-    return authbroker_client
+        return oauth.authbroker
 
 
 def _is_safe_url(target_url):
@@ -113,11 +103,24 @@ def protect_all_views(exclude=None):
     _protect_all_views = current_app.before_request(_protect_all_views)
 
 
+def is_authenticated():
+    """Is the current user authenticated with the auth broker?
+
+    TODO: add configurable grace period to avoid verifying on every request"""
+    if TOKEN_SESSION_KEY in session:
+        me = _get_client().get(
+            urljoin(current_app.config['ABC_BASE_URL'], PROFILE_PATH)
+        )
+        if me.ok:
+            return True
+
+    return False
+
+
 def get_profile():
     """Get the user's profile"""
-    response = _get_client().get(PROFILE_PATH)
-
-    if response.status != 200:
+    response = _get_client().get(urljoin(current_app.config['ABC_BASE_URL'], PROFILE_PATH))
+    if response.status_code != 200:
         raise NotAuthenticatedError
 
     return response.json()
@@ -126,18 +129,6 @@ def get_profile():
 def logout_user():
     """Kill the session"""
     session.pop(TOKEN_SESSION_KEY, None)
-
-
-def is_authenticated():
-    """Is the current user authenticated with the auth broker?
-
-    TODO: add configurable grace period to avoid verifying on every request"""
-    if TOKEN_SESSION_KEY in session:
-        me = _get_client().get(PROFILE_PATH)
-        if me.status == 200:
-            return True
-
-    return False
 
 
 @authbroker_blueprint.route('/login')
@@ -151,7 +142,25 @@ def login():
         # by _get_next_url() when the user returns from ABC
         session['next'] = next_url
 
-    return _get_client().authorize(callback=url_for('auth.authorised', _external=True))
+    return _get_client().authorize_redirect(
+        url_for('auth.authorised', _external=True)
+    )
+
+
+@authbroker_blueprint.route('/login/authorised')
+def authorised():
+    """The authorise view"""
+
+    access_token = _get_client().authorize_access_token()
+
+    if access_token is None:
+        return 'Access denied: reason=%s error=%s resp=%s' % (
+            request.args['error'],
+            request.args['error_description'],
+            access_token)
+    else:
+        session[TOKEN_SESSION_KEY] = access_token
+        return redirect(_get_next_url())
 
 
 @authbroker_blueprint.route('/logout')
@@ -162,22 +171,4 @@ def logout():
     return redirect('/')
 
 
-@authbroker_blueprint.route('/login/authorised')
-def authorised():
-    """The authorise view"""
 
-    resp = _get_client().authorized_response()
-
-    if resp is None or resp.get('access_token') is None:
-        return 'Access denied: reason=%s error=%s resp=%s' % (
-            request.args['error'],
-            request.args['error_description'],
-            resp)
-    else:
-        session[TOKEN_SESSION_KEY] = (resp['access_token'], '')
-        return redirect(_get_next_url())
-
-
-def get_token():
-    """Required by OAuth client - this is wired up in `_get_client()`"""
-    return session.get(TOKEN_SESSION_KEY)
